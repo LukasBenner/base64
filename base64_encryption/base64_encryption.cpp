ea63ab4dd3e8c33fa38dff71a2e851f3348a4c60
Lukas Benner <Lukas.Benner@MITUTOYO-CTL.DE>
2021-03-18 17:23:19 +0100

    Added a file to encode

--- base64_encryption/base64_encryption.cpp
+++ base64_encryption/base64_encryption.cpp
@@ -2,14 +2,25 @@
 #include <string>
 #include "decoder.h"
 #include "encoder.h"
-
+#include <fstream>
+#include <iostream>
 
 int main()
 {
+	
+	
 	std::string input;
-	std::getline(std::cin, input);
+	//std::getline(std::cin, input);
 
+	std::ifstream readfile("F:\\Projekte\\base64_encryption\\base64_encryption\\planetext.txt");
+	std::string line;
+	
 	std::cout << "Press e for encoding, press d for decoding\n";
+	while (std::getline(readfile, line)) {
+		// Output the text from the file
+		input.append(line);
+		input.push_back('\n');
+	}
 	char decision;
 	std::cin >> decision;
 
