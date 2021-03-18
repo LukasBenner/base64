73904c3686dadce7a3b9632e68716e63cd7d9f49
Lukas Benner <Lukas.Benner@MITUTOYO-CTL.DE>
2021-03-18 11:54:19 +0100

    Added a Function that turns a string into a vector holding strings with the length 3

--- base64_encryption/helperFunctions.h
+++ base64_encryption/helperFunctions.h
@@ -11,3 +11,27 @@ float getSize(char* text)
 	}
 	return size;
 }
+
+
+std::vector<std::string> convert_to_three_byte_wise(std::string in)
+{
+	std::vector<std::string> three_byte_wise;
+	uint8_t counter = 0;
+	std::string three_byte = "";
+	for (auto letter : in)
+	{
+		three_byte.push_back(letter);
+		counter++;
+		if (counter % 3 == 0)
+		{
+			three_byte_wise.push_back(three_byte);
+			three_byte = "";
+			counter = 0;
+		}
+	}
+	while (three_byte_wise.back().length() < 3)
+	{
+		three_byte_wise.back().push_back('\0');
+	}
+	return three_byte_wise;
+}
--- base64_encryptionUnitTest/base64_encryptionUnitTest.cpp
+++ base64_encryptionUnitTest/base64_encryptionUnitTest.cpp
@@ -12,11 +12,25 @@ namespace base64encryptionUnitTest
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(TestEncode1)
 		{
 			char* in = "Hallo";
 			std::string out = "SGFsbG8=";
-			Assert::IsTrue(out.compare(encode(in)) == 0);
+			Assert::IsTrue(out == encode(in));
+		}
+
+		TEST_METHOD(TestDecode1)
+		{
+			char* in = "RGllcyBpc3QgZWluIFRlc3QgZGVyIERlY29kZXIgc29sbCBnZXRlc3RldCB3ZXJkZW4u";
+			std::string out = "Dies ist ein Test der Decoder soll getestet werden.";
+			Assert::IsTrue(out == decode(in));
+		}
+
+		TEST_METHOD(TestConvert_to_vector)
+		{
+			std::string in = "Hallo Welt";
+			std::vector<std::string> out = { "Hal", "lo ", "Wel", "t\0\0" };
+			
 		}
 	};
 }
